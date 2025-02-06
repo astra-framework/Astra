f7fc2aa7ca67674d56a4ba1c445839920623dbd3
atomsk-0 <wexberkyhd@gmail.com>
2025-02-06 20:03:47 +0200

    Added ContextMenu

---
+++ Astra/Components/ContextMenu.cs
@@ -0,0 +1,24 @@
+﻿using Astra.Styles;
+using Astra.Util;
+using Hexa.NET.ImGui;
+
+namespace Astra.Components;
+
+public static class ContextMenu
+{
+    public static void Normal(string name, in ContextMenuStyle style, Action content)
+    {
+        ImGui.PushStyleVar(ImGuiStyleVar.WindowPadding, style.Padding);
+        ImGui.PushStyleVar(ImGuiStyleVar.PopupRounding, style.Radius);
+        ImGui.PushStyleVar(ImGuiStyleVar.PopupBorderSize, style.BorderThickness);
+        ImGui.PushStyleColor(ImGuiCol.PopupBg, style.BackgroundColor.ToUint32());
+        ImGui.PushStyleColor(ImGuiCol.Border, style.BorderColor.ToUint32());
+        if (ImGui.BeginPopupContextItem(name))
+        {
+            content.Invoke();
+            ImGui.EndPopup();
+        }
+        ImGui.PopStyleColor(2);
+        ImGui.PopStyleVar(3);
+    }
+}
---
+++ Astra/Styles/ContextMenuStyles.cs
@@ -0,0 +1,15 @@
+﻿using System.Drawing;
+using System.Numerics;
+
+namespace Astra.Styles;
+
+public struct ContextMenuStyle
+{
+    public Vector2 Padding;
+
+    public Color BackgroundColor;
+    public Color BorderColor;
+
+    public float Radius;
+    public float BorderThickness;
+}
